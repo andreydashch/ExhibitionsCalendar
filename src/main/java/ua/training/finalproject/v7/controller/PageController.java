e4ac1e3b2c9b99b010da44a78fdb2dc2211285e1
andreydashch <andreydashch@gmail.com>
2020-01-07 15:44:57 +0200

    Add PageController

--- src/main/java/ua/training/finalproject/v7/controller/PageController.java
+++ src/main/java/ua/training/finalproject/v7/controller/PageController.java
@@ -30,18 +30,19 @@
  */
 
 
-package ua.training.finalproject.v7;
+package ua.training.finalproject.v7.controller;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * Date: 07/01/2020
  * User: Andrey Dashchyk
  */
-@SpringBootApplication
-public class App {
-    public static void main(String[] args) {
-        SpringApplication.run(App.class, args);
+@Controller
+public class PageController {
+    @RequestMapping("/")
+    public String registrationPage() {
+        return "registrationPage.html";
     }
 }
